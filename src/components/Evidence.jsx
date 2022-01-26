b2a2d6582f4d5e43e2b63cca01b97f9152ea266d
klappy <christopher@klapp.name>
2022-01-25 19:47:14 -0500

    remove optimizations and useState for randomWords

--- src/components/Evidence.jsx
+++ src/components/Evidence.jsx
@@ -1,28 +1,25 @@
-import React, { useState, useMemo } from "react";
-import { useDeepCompareMemo, useDeepCompareEffect as useEffect } from "use-deep-compare";
+import React, { useState } from "react";
+import { useDeepCompareEffect } from "use-deep-compare";
 import Word from "./Word";
 
 export default function Evidence({ evidenceText = '', editorText = '' }) {
   const [randomWords, setRandomWords] = useState([]);
 
-  const evidenceWords = useMemo(() => {
-    return evidenceText.split(' ');
-  }, [evidenceText]);
+  const evidenceWords = evidenceText.split(' ');
 
-  useEffect(() => {
+  useDeepCompareEffect(() => {
     const _randomWords = evidenceWords.sort(() => (Math.random() > .5) ? 1 : -1);
     setRandomWords(_randomWords);
   }, [evidenceWords]);
 
-  const wordsComponent = useDeepCompareMemo(() => {
-    const editorWords = editorText.split(' ');
-    const correctOverlap = editorWords.filter((word, index) => (word === evidenceWords[index]));
-    return randomWords.map((word, index) => {
-      const used = editorWords.includes(word);
-      const correct = (correctOverlap.includes(word));
-      return (<Word key={word + index} used={used} text={word} correct={correct} />);
-    });
-  }, [evidenceWords, editorText, randomWords]);
+  const editorWords = editorText.split(' ');
+  const correctOverlap = editorWords.filter((word, index) => (word === evidenceWords[index]));
+
+  const wordsComponent = randomWords.map((word, index) => {
+    const used = editorWords.includes(word);
+    const correct = (correctOverlap.includes(word));
+    return (<Word key={word + index} used={used} text={word} correct={correct} />);
+  });
 
   return (
     <div className="Evidence">
