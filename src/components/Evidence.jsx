a760f428c01cf17c5074bdd98427785bc9e369b5
klappy <christopher@klapp.name>
2022-01-25 19:43:06 -0500

    useDeepCompareMemo

--- src/components/Evidence.jsx
+++ src/components/Evidence.jsx
@@ -1,18 +1,20 @@
-import React, { useState } from "react";
-import { useDeepCompareMemo as useMemo, useDeepCompareEffect as useEffect } from "use-deep-compare";
+import React, { useState, useMemo } from "react";
+import { useDeepCompareMemo, useDeepCompareEffect as useEffect } from "use-deep-compare";
 import Word from "./Word";
 
 export default function Evidence({ evidenceText = '', editorText = '' }) {
   const [randomWords, setRandomWords] = useState([]);
 
-  const evidenceWords = evidenceText.split(' ');
+  const evidenceWords = useMemo(() => {
+    return evidenceText.split(' ');
+  }, [evidenceText]);
 
   useEffect(() => {
     const _randomWords = evidenceWords.sort(() => (Math.random() > .5) ? 1 : -1);
     setRandomWords(_randomWords);
   }, [evidenceWords]);
 
-  const wordsComponent = useMemo(() => {
+  const wordsComponent = useDeepCompareMemo(() => {
     const editorWords = editorText.split(' ');
     const correctOverlap = editorWords.filter((word, index) => (word === evidenceWords[index]));
     return randomWords.map((word, index) => {
